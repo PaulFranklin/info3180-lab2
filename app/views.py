a176864306ca61a26de8c8ed61735d1f956cc634
Paul Franklin <paul.franklin@mymona.uwi.edu>
2017-02-06 02:16:19 +0000

    final commit (profile.html not showing when the python app is ran and url_for isnt showing my picture

--- app/views.py
+++ app/views.py
@@ -8,10 +8,17 @@ This file creates your application.
 from app import app
 from flask import render_template, request, redirect, url_for
 
+from datetime import datetime, date
+import time
+from time import strftime
+
 
 ###
 # Routing for your application.
 ###
+
+
+
 
 @app.route('/')
 def home():
@@ -23,6 +30,19 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+     
+
+    
+@app.route('/profile/')
+def profile ():
+    """Render profile"""
+    return render_template('profile.html',time_info= timeinfo() )
+    
+@app.route ('/timeinfo/')
+def timeinfo ():
+    
+    """  timeinfo"""
+    return datetime.now().strftime('%A/%w/%b/%Y')
 
 
 ###
@@ -51,6 +71,8 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+    
+
 
 
 if __name__ == '__main__':
